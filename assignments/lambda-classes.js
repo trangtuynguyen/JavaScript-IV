a9b269177216dc0537dd7f9797e4588141b7f2d9
Trang Nguyen <ttn2127@columbia.edu>
2019-06-13 15:43:04 -0400

    person, instructor class done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+
+class Person{
+    constructor(pAttr){
+        this.name = pAttr.name;
+        this.age = pAttr.age;
+        this.location = pAttr.location;
+    }
+
+    speak(){
+        console.log(`Hello my name is ${this.name} and I am from ${this.location}`);
+    }
+}
